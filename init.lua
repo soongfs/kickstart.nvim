6d6f1dfea83124c5dfc93172f436750b34ebc595
soongfs <soongfs04@gmail.com>
2026-02-15 13:31:04 +0800

    Enable italic

--- init.lua
+++ init.lua
@@ -807,6 +807,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.g.gruvbox_material_enable_italic = true
       vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
